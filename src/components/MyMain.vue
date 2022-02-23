56e6ea580104bd448a6ad119f99a36460f91e6f4
Matteo Gioele Cocomazzi <TheZefyro@gmail.com>
2022-02-23 18:16:43 +0100

    push con props

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -1,18 +1,22 @@
 <template>
     <div class="container">
-  <div class="row row-cols-2 row-cols-sm-3 row-cols-lg-5 justify-content-center">
+        <div class="row row-cols-2 row-cols-sm-3 row-cols-lg-5 justify-content-center">
+            <AlbumSingolo
+            v-for="(album, indice) in listaAlbum" 
+            :key="indice"
+            :album="album"
+            >
+            </AlbumSingolo>
+            <!-- <div class="text-center" v-for="(album, indice) in listaAlbum" :key="indice">
+                <div class="ms_box">
+                    <img class="ms_img-fluid" :src="album.poster" alt="">
+                    <h3>{{album.title}}</h3>
+                    <h5>{{album.author}}</h5>
+                    <h5>{{album.year}}</h5>
+                </div>  
+            </div> -->
 
-    <div class="text-center" v-for="(album, indice) in listaAlbum" :key="indice">
-        <div class="ms_box">
-            <img class="ms_img-fluid" :src="album.poster" alt="">
-            <h3>{{album.title}}</h3>
-            <h5>{{album.author}}</h5>
-            <h5>{{album.year}}</h5>
         </div>
-      
-    </div>
-
-  </div>
 </div>
 </template>
 
@@ -20,6 +24,7 @@
 
     // integro axios
     const axios = require('axios');
+    import AlbumSingolo from './partials/AlbumSingolo.vue'
 
 export default {
     name: 'MyMain',
@@ -27,6 +32,9 @@ export default {
         return{
             listaAlbum: []
         }
+    },
+    components:{
+        AlbumSingolo,
     },
     methods:{
         getAlbum(){
@@ -53,16 +61,16 @@ export default {
 </script>
 
 <style scoped lang="scss">
-    .ms_img-fluid{
-        margin: 10px 0px;
-        width: 90%;
-}
+    // .ms_img-fluid{
+    //     margin: 10px 0px;
+    //     width: 90%;
+    // }
 
-    .ms_box{
-        width: 200px;
-        min-height: 380px;
-        background-color: #2e3a46;
-        margin: 30px;
-        padding: 5px;
-    }
+    // .ms_box{
+    //     width: 200px;
+    //     min-height: 380px;
+    //     background-color: #2e3a46;
+    //     margin: 30px;
+    //     padding: 5px;
+    // }
 </style>
---
+++ src/components/partials/AlbumSingolo.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="text-center">
+        <div class="ms_box">
+            <img class="ms_img-fluid" :src="album.poster" alt="">
+            <h3>{{album.title}}</h3>
+            <h5>{{album.author}}</h5>
+            <h5>{{album.year}}</h5>
+        </div>  
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AlbumSingolo',
+    props:{
+        "album": Object
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    .ms_img-fluid{
+        margin: 10px 0px;
+        width: 90%;
+    }
+
+    .ms_box{
+        width: 200px;
+        min-height: 380px;
+        background-color: #2e3a46;
+        margin: 30px;
+        padding: 5px;
+    }
+</style>
